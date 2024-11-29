e7171e5d45bc3419e727a2cea7472385b40e42d6
Jlmerinocruz <a00840956@tec.mx>
2024-11-29 09:43:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
-int skibidi = 1;
+#include <iostream>
+using namespace std;
+int main(){
+  int n, num;
+  double sum;
+  cout << "How many numbers are you going to type?";
+  cin >> n;
+  return 0;
+}
